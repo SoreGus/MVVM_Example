c187b675f3e30805782c0a1a7d114f22a3f050d5
Unknown <sore.gustavo@gmail.com>
2019-10-29 22:07:37 -0300

    Setup Xcode project

---
+++ MVVMExample/MVVMExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVMExample
+//
+//  Created by Gustavo Luís Soré on 29/10/19.
+//  Copyright © 2019 Sore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
